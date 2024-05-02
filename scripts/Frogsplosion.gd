7d10a356ecb2ae68da4dea8071b91445479c8ab8
Sebastien M <611890+Xyloph@users.noreply.github.com>
2024-05-02 07:26:48 -0400

    skipped a for loop

--- scripts/Frogsplosion.gd
+++ scripts/Frogsplosion.gd
@@ -22,18 +22,17 @@ func _process(delta: float) -> void:
 
 # set visible and start animation
 func play(direction: Vehicule.going) -> void:
-	# pick up target points
-	var targets : Array[Vector2] = []
-	for i in range(5):
-		targets.append(Vector2(1 if direction == Vehicule.going.right else -1, randf_range(-0.5,0.5))*randfn(400.0,100.0))
-	
 	# show ourselves
 	show()
 	
 	# animate
 	var tw := get_tree().create_tween().set_parallel().set_ease(Tween.EASE_IN)
 	for i in range(5):
-		tw.tween_property(parts[i], "position", targets[i], animation_speed)
+		# pick a random point in the direction we were hit and move to it
+		tw.tween_property(
+			parts[i], "position", 
+			Vector2(1 if direction == Vehicule.going.right else -1, randf_range(-0.5,0.5))*randfn(400.0,100.0),
+			animation_speed)
 		tw.tween_property(parts[i], "rotation", randf_range(0.0, 3.0*TAU), animation_speed)
 	
 	# call the second part of the animation on another tween
